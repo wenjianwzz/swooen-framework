3d7429d5bbfa3e95d4866485dd0850ec8cc11e2e
wenjianwzz <wenjianwzz@126.com>
2021-07-09 09:59:36 +0800

    fixed

--- src/Server/Swoole/Redis/RedisConnectionFactory.php
+++ src/Server/Swoole/Redis/RedisConnectionFactory.php
@@ -13,13 +13,182 @@ use Swooen\Server\Http\Writer\JsonWriter;
  * @author WZZ
  */
 class RedisConnectionFactory implements ConnectionFactory {
-	
-	public function make() {
-		$connection = new RedisConnection();
-		$reader = new HttpReader();
-		$connection->instance(Writer::class, new JsonWriter());
-		$connection->instance(Reader::class, $reader);
-		return $connection;
+
+	protected $server;
+
+	protected $callback;
+
+	public function __construct($host, $porrt, $mode=SWOOLE_BASE, $sockType=NULL) {
+		$this->server = new \Swoole\Redis\Server($host, $porrt, $mode, $sockType);
+		foreach (self::COMMANDS as $cmd) {
+			$this->server->setHandler($cmd, function($fd, $data) {
+				var_dump($data);
+			});
+		}
 	}
+		
+	public function onConnection(callable $callback) {
+		$this->callback = $callback;
+	}
+	
+	public function start() {
+		$this->server->start();
+	}
+
+	const COMMANDS = [
+		'DEL',
+		'DUMP',
+		'EXISTS',
+		'EXPIRE',
+		'EXPIREAT',
+		'KEYS',
+		'MIGRATE',
+		'MOVE',
+		'OBJECT',
+		'PERSIST',
+		'PEXPIRE',
+		'PEXPIREAT',
+		'PTTL',
+		'RANDOMKEY',
+		'RENAME',
+		'RENAMENX',
+		'RESTORE',
+		'SORT',
+		'TTL',
+		'TYPE',
+		'SCAN',
+		'APPEND',
+		'BITCOUNT',
+		'BITOP',
+		'DECR',
+		'DECRBY',
+		'GET',
+		'GETBIT',
+		'GETRANGE',
+		'GETSET',
+		'INCR',
+		'INCRBY',
+		'INCRBYFLOAT',
+		'MGET',
+		'MSET',
+		'MSETNX',
+		'PSETEX',
+		'SET',
+		'SETBIT',
+		'SETEX',
+		'SETNX',
+		'SETRANGE',
+		'STRLEN',
+		'HDEL',
+		'HEXISTS',
+		'HGET',
+		'HGETALL',
+		'HINCRBY',
+		'HINCRBYFLOAT',
+		'HKEYS',
+		'HLEN',
+		'HMGET',
+		'HMSET',
+		'HSET',
+		'HSETNX',
+		'HVALS',
+		'HSCAN',
+		'BLPOP',
+		'BRPOP',
+		'BRPOPLPUSH',
+		'LINDEX',
+		'LINSERT',
+		'LLEN',
+		'LPOP',
+		'LPUSH',
+		'LPUSHX',
+		'LRANGE',
+		'LREM',
+		'LSET',
+		'LTRIM',
+		'RPOP',
+		'RPOPLPUSH',
+		'RPUSH',
+		'RPUSHX',
+		'SADD',
+		'SCARD',
+		'SDIFF',
+		'SDIFFSTORE',
+		'SINTER',
+		'SINTERSTORE',
+		'SISMEMBER',
+		'SMEMBERS',
+		'SMOVE',
+		'SPOP',
+		'SRANDMEMBER',
+		'SREM',
+		'SUNION',
+		'SUNIONSTORE',
+		'SSCAN',
+		'ZADD',
+		'ZCARD',
+		'ZCOUNT',
+		'ZINCRBY',
+		'ZRANGE',
+		'ZRANGEBYSCORE',
+		'ZRANK',
+		'ZREM',
+		'ZREMRANGEBYRANK',
+		'ZREMRANGEBYSCORE',
+		'ZREVRANGE',
+		'ZREVRANGEBYSCORE',
+		'ZREVRANK',
+		'ZSCORE',
+		'ZUNIONSTORE',
+		'ZINTERSTORE',
+		'ZSCAN',
+		'PSUBSCRIBE',
+		'PUBLISH',
+		'PUBSUB',
+		'PUNSUBSCRIBE',
+		'SUBSCRIBE',
+		'UNSUBSCRIBE',
+		'DISCARD',
+		'EXEC',
+		'MULTI',
+		'UNWATCH',
+		'WATCH',
+		'EVAL',
+		'EVALSHA',
+		'SCRIPT EXISTS',
+		'SCRIPT FLUSH',
+		'SCRIPT KILL',
+		'SCRIPT LOAD',
+		'AUTH',
+		'ECHO',
+		'PING',
+		'QUIT',
+		'SELECT',
+		'BGREWRITEAOF',
+		'BGSAVE',
+		'CLIENT GETNAME',
+		'CLIENT KILL',
+		'CLIENT LIST',
+		'CLIENT SETNAME',
+		'CONFIG GET',
+		'CONFIG RESETSTAT',
+		'CONFIG REWRITE',
+		'CONFIG SET',
+		'DBSIZE',
+		'DEBUG OBJECT',
+		'DEBUG SEGFAULT',
+		'FLUSHALL',
+		'FLUSHDB',
+		'INFO',
+		'LASTSAVE',
+		'MONITOR',
+		'PSYNC',
+		'SAVE',
+		'SHUTDOWN',
+		'SLAVEOF',
+		'SLOWLOG',
+		'SYNC',
+		'TIME'
+	];
 
 }
