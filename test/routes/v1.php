32c16360d5964cfe5a38e0e7795ba74842af871e
wenjianwzz <wenjianwzz@126.com>
2022-11-11 12:40:10 +0000

    路由

--- test/routes/v1.php
+++ test/routes/v1.php
@@ -5,7 +5,7 @@ use Swooen\Handle\Writer\Writer;
 use Swooen\Package\RawPackage;
 
 return [
-    Route::create('/test', function(Route $route, Writer $writer, $path) {
-        $writer->send(new RawPackage($path));
+    Route::create('GET /test', function(Route $route, Writer $writer) {
+        $writer->send(new RawPackage($route->getPath()));
     })
 ];
